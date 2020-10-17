64d8f6fc9447c66285eec4b68111fd6c31ea8f15
obezglavlen <pcgalaxy123@gmail.com>
2020-10-17 17:33:15 +0300

    Form Factor of folders

---
+++ ChatBot_Kursach/src/Algorithm/Algorithm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot_Kursach.Algorithms
+{
+    public class Algorithm
+    {
+        public Algorithm() { }
+        public int b { get; set; }
+        
+    }
+}
--- ChatBot_Kursach/src/Form/Form1.cs
+++ ChatBot_Kursach/src/Form/Form1.cs
@@ -8,18 +8,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ChatBot_Kursach.Algorithms;
 
 namespace ChatBot_Kursach
 {
     public partial class Form1 : Form
     {
+        //
+        // Добавление екземпляра класса Algorithm
+        //
+        Algorithm Test = new Algorithm();
+
         public Form1()
         {
             InitializeComponent();
         }
-private void button1_Click_1(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
         {
+
+            Test.b = 331;
             string str = textBox1.Text;
+            textBox2.Text = $"{Convert.ToString(Test.b)}" + $" + {str}"; // Конвертация Test.b в string и добавление к нему теста из поля ввода
+
             //pictureBox1.Image = (Image)Resource1.ResourceManager.GetObject(yourClass.getNeedImage());
         }
 
